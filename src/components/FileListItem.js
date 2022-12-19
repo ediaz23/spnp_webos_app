8392a0b7d2f35b67080e6b5468b52a0ec426d037
ediaz23 <esteban.diazt23@gmail.com>
2022-12-19 13:30:50 -0300

    [IMP] Visual fix

--- src/components/FileListItem.js
+++ src/components/FileListItem.js
@@ -3,25 +3,11 @@ import kind from '@enact/core/kind'
 import PropTypes from 'prop-types'
 import Item from '@enact/moonstone/Item'
 import Image from '@enact/moonstone/Image'
-import BodyText from '@enact/moonstone/BodyText'
-import { Row, Cell } from '@enact/ui/Layout'
+import { Row, Cell, Column } from '@enact/ui/Layout'
 import css from './FileListItem.module.less'
 
 
 const cleanDuration = str => str.split('.')[0]
-/** @param {import('../models/File').default} file */
-const simple = file => (
-    <div className={css.caption}>
-        <BodyText>{file.title}</BodyText>
-    </div>
-)
-/** @param {import('../models/Playable').default} file */
-const playable = file => (
-    <div className={css.caption}>
-        <BodyText>{file.title}</BodyText>
-        <BodyText>{cleanDuration(file.res.duration)}</BodyText>
-    </div>
-)
 
 const FileListItem = kind({
     name: 'FileListItem',
@@ -41,8 +27,11 @@ const FileListItem = kind({
                             style={{ maxHeight: 100, maxWidth: 100 }} />
                     </Cell>
                     <Cell align="center">
-                        {['folder', 'image'].includes(file.type) && simple(file)}
-                        {['video', 'music'].includes(file.type) && playable(file)}
+                        <Column>
+                            <Cell>{file.title}</Cell>
+                            {['video', 'music'].includes(file.type) &&
+                                <Cell>{cleanDuration(file.res.duration)}</Cell>}
+                        </Column>
                     </Cell>
                 </Row>
             </Item>
