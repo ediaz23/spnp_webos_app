705e7d52b307e15430c6a335c4343e5fa88cc0a1
ediaz23 <esteban.diazt23@gmail.com>
2022-12-19 14:19:02 -0300

    [IMP] Refact file list item

--- src/components/FileListItem.js
+++ src/components/FileListItem.js
@@ -1,42 +1,43 @@
 
-import kind from '@enact/core/kind'
 import PropTypes from 'prop-types'
-import Item from '@enact/moonstone/Item'
 import Image from '@enact/moonstone/Image'
+import Marquee from '@enact/moonstone/Marquee'
+import Spottable from '@enact/spotlight/Spottable'
+import Skinnable from '@enact/moonstone/Skinnable'
 import { Row, Cell, Column } from '@enact/ui/Layout'
 import css from './FileListItem.module.less'
 
 
 const cleanDuration = str => str.split('.')[0]
+const CellItem = Spottable(Skinnable(Cell))
 
-const FileListItem = kind({
-    name: 'FileListItem',
+const FileListItem = ({ selectItem, file, ...rest }) => {
 
-    propTypes: {
-        selectItem: PropTypes.func,
-        file: PropTypes.object
-    },
+    return (
+        <CellItem onClick={selectItem} {...rest}>
+            <Row>
+                <Cell shrink>
+                    <Image className={css.image}
+                        src={file.imageUrl}
+                        style={{ maxHeight: 100, maxWidth: 100 }} />
+                </Cell>
+                <Cell align="center">
+                    <Column>
+                        <Cell>
+                            <Marquee marqueeOn='render'>{file.title}</Marquee>
+                        </Cell>
+                        {['video', 'music'].includes(file.type) &&
+                            <Cell>{cleanDuration(file.res.duration)}</Cell>}
+                    </Column>
+                </Cell>
+            </Row>
+        </CellItem>
+    )
+}
 
-    render: ({ selectItem, file, ...rest }) => {
-        return (
-            <Item onClick={selectItem} {...rest}>
-                <Row>
-                    <Cell shrink>
-                        <Image className={css.image}
-                            src={file.imageUrl}
-                            style={{ maxHeight: 100, maxWidth: 100 }} />
-                    </Cell>
-                    <Cell align="center">
-                        <Column>
-                            <Cell>{file.title}</Cell>
-                            {['video', 'music'].includes(file.type) &&
-                                <Cell>{cleanDuration(file.res.duration)}</Cell>}
-                        </Column>
-                    </Cell>
-                </Row>
-            </Item>
-        )
-    }
-})
+FileListItem.propTypes = {
+    selectItem: PropTypes.func,
+    file: PropTypes.object
+}
 
 export default FileListItem
